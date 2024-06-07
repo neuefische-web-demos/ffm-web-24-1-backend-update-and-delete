e16f87e39823978928885e3f61fb7690d58d339f
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-06-07 10:34:44 +0200

    initial commit

---
+++ components/JokeForm/index.js
@@ -0,0 +1,12 @@
+export default function JokeForm({ value, onSubmit, isEditMode }) {
+
+  return (
+    <form style={{border: "2px solid green"}}onSubmit={onSubmit}>
+      <label htmlFor="joke-input">
+        {isEditMode ? "Edit the joke" : "Enter a new joke"}
+      </label>
+      <input type="text" id="joke-input" name="joke" defaultValue={value} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
---
+++ pages/api/jokes/[id].js
@@ -0,0 +1,28 @@
+import dbConnect from "../../../db/connect";
+import Joke from "../../../db/models/Joke";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    const joke = await Joke.findById(id);
+
+    if (!joke) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(joke);
+  }
+
+  if (request.method === "PUT") {
+    const jokeData = request.body;
+    await Joke.findByIdAndUpdate(id, jokeData);
+    response.status(200).json({ status: "Joke updated" });
+  }
+
+  if (request.method === "DELETE"){
+    await Joke.findByIdAndDelete(id);
+    response.status(200).json({ status: "Joke deleted" });
+  }
+}
